bd0b4fc81eedd79f1667f161d447078dfadd4c0b
CristianPerafan <1110282139@u.icesi.edu.co>
2022-04-15 18:35:43 -0500

    First real commit

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,10 +3,9 @@ package ui;
 public class Main {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hello world");
-		System.out.println("Hola Carlos y Cristian");
-		System.out.println("Odio estructuras discretas");
-		System.out.println("Holaa =)");
+
 	}
 
 }
